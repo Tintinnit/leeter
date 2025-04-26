e308c4f973230a8958ace278fb161d651ff4b110
Tintin <oofedindeed@gmail.com>
2025-04-25 23:45:48 -0400

    April 25th 2025 13 Roman to Integer.

---
+++ April/13.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 13. Roman to Integer
+
+class Solution {
+    public:
+        int romanToInt(string s) {
+            int len = s.length(); 
+            int total = 0;
+
+            for (int i = 0; i < len; i++) {
+                if (s[i] == 'I') {
+                    if (s[i + 1] == 'V') {
+                        total += 4;
+                        i++;
+                    } else if (s[i + 1] == 'X') {
+                        total += 9;
+                        i++;
+                    } else {
+                        total += 1;
+                    }
+                } else if (s[i] == 'V') {
+                    total += 5;
+                } else if (s[i] == 'X') {
+                    if (s[i + 1] == 'L') {
+                        total += 40;
+                        i++;
+                    } else if (s[i + 1] == 'C') {
+                        total += 90;
+                        i++;
+                    } else {
+                        total += 10;
+                    }
+                } else if (s[i] == 'L') {
+                    total += 50;
+                } else if (s[i] == 'C') {
+                    if (s[i + 1] == 'D') {
+                        total += 400;
+                        i++;
+                    } else if (s[i + 1] == 'M') {
+                        total += 900;
+                        i++;
+                    } else {
+                        total += 100;
+                    }
+                } else if (s[i] == 'D') {
+                    total += 500;
+                } else {
+                    total += 1000;
+                }
+            }
+
+            return total;
+        }
+};
+
+int main () {
+    return 0;
+}
