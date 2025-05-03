a0f4dac2bea70dc47c7e070551226d4f7acc2680
Tintin <oofedindeed@gmail.com>
2025-05-02 23:23:22 -0400

    May 2nd 2025 838 Push Dominoes.

---
+++ May/838.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 838. Push Dominoes
+
+class Solution {
+    public:
+        string pushDominoes(string dominoes) {
+            int n = dominoes.size();
+            vector<int> forces(n, 0);
+            
+            // Left to right pass
+            int force = 0;
+            for (int i = 0; i < n; i++) {
+                if (dominoes[i] == 'R') {
+                    force = n;
+                } else if (dominoes[i] == 'L') {
+                    force = 0;
+                } else {
+                    force = max(force - 1, 0);
+                }
+                forces[i] += force;
+            }
+    
+            // Right to left pass
+            force = 0;
+            for (int i = n - 1; i >= 0; i--) {
+                if (dominoes[i] == 'L') {
+                    force = n;
+                } else if (dominoes[i] == 'R') {
+                    force = 0;
+                } else {
+                    force = max(force - 1, 0);
+                }
+                forces[i] -= force;
+            }
+    
+            // Build result
+            string result = "";
+            for (int f : forces) {
+                if (f > 0) result += 'R';
+                else if (f < 0) result += 'L';
+                else result += '.';
+            }
+    
+            return result;
+        }
+    };
+    
+
+int main() {
+    Solution solution;
+
+    return 0;
+}
