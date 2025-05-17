b794bf5b57ce7eb2ddd3ac363c2a08b31468238f
Tintin <oofedindeed@gmail.com>
2025-05-17 00:44:27 -0400

    May 17th 2025 Full solution for 3335.

--- May/3335.cpp
+++ May/3335.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <iostream>
 
 using namespace std;
 
@@ -9,33 +10,51 @@ using namespace std;
 class Solution {
     public:
         const int MOD = 1e9 + 7;
-    
+        int mod_add(int a, int b) {a = a % MOD; b = b % MOD; return (((a + b) % MOD) + MOD) % MOD;}
+        
         int lengthAfterTransformations(string s, int t) {
-        unordered_map<char, long long> count;
-        for (char c : s) {
-            count[c]++;
-        }
-    
-        for (int i = 0; i < t; ++i) {
-            unordered_map<char, long long> new_count;
-            for (auto& pair : count) {
-                char c = pair.first;
-                long long cnt = pair.second;
-                if (c == 'z') {
-                    new_count['a'] += cnt;
-                    new_count['b'] += cnt;
-                } else {
-                    char next_char = c + 1;
-                    new_count[next_char] += cnt;
+            int nums[26] = {0};
+            for (char ch : s) nums[ch - 'a']++;
+            while (t--) {
+                int cur[26] = {0};
+                for (int j = 0; j < 26; j++) {
+                    if (j == 25 && nums[j] > 0) {
+                        cur[0] = mod_add(cur[0], nums[j]);
+                        cur[1] = mod_add(cur[1], nums[j]);
+                    } else {
+                        if (j < 25) cur[j + 1] = mod_add(cur[j + 1], nums[j]);
+                    }
                 }
+                for (int j = 0; j < 26; j++) nums[j] = cur[j];
             }
-            count = new_count;
-        }
-    
-        long long total = 0;
-        for (auto& pair : count) {
-            total += pair.second;
-        }
-        return total % MOD;
+            int ans = 0;
+            for (int i : nums) ans = mod_add(ans, i);
+            return (int)ans;
         }
 };
+
+int main() {
+    Solution sol;
+
+    // Test Case 1
+    string s1 = "abc";
+    int t1 = 1;
+    cout << "Test Case 1: " << sol.lengthAfterTransformations(s1, t1) << endl;
+
+    // Test Case 2
+    string s2 = "z";
+    int t2 = 2;
+    cout << "Test Case 2: " << sol.lengthAfterTransformations(s2, t2) << endl;
+
+    // Test Case 3
+    string s3 = "az";
+    int t3 = 3;
+    cout << "Test Case 3: " << sol.lengthAfterTransformations(s3, t3) << endl;
+
+    // Test Case 4 - edge case with t = 0
+    string s4 = "abc";
+    int t4 = 0;
+    cout << "Test Case 4: " << sol.lengthAfterTransformations(s4, t4) << endl;
+
+    return 0;
+}
